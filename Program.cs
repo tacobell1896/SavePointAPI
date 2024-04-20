a9f282b37ee17e9bee4c0745ec2d183744ea1b6b
Turner Bell <turner@tbell.us>
2024-04-19 20:33:09 -0400

    add connection to postgresql

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using SavePointAPI.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("SavePoint");
 // TODO: Use a database for production scenarios and configure the connection string in appsettings.json
 builder.Services.AddDbContext<SavePointContext>(options =>
-    options.UseInMemoryDatabase("SavePointList"));
+    options.UseNpgsql(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
